5cf93c10658c953a2d062eb635d469267c7016ba
SerhiiKuzmenko <110684653+SerhiiKuzmenko@users.noreply.github.com>
2022-09-08 10:16:45 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,109 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('#info .mainfo').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDown");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.one').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.two').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.three').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('#mail .mainfo').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("rollIn");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.a').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("rotateInDownLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.b').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("rotateInDownRight");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.c').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("rotateInDownLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.d').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("rotateInDownRight");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.bwidth').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("rubberBand");
+	        }
+	    });
+	});
+})
+var btn = document.getElementById('btn');
+
+btn.onclick = function (e) {
+	e.preventDefault();
+	var text = document.querySelector('.second');
+	text.classList.add('red');
+}
